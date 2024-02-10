7dbdb1ba971dce7a77f0206d2992353e2c794f0b
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2024-02-10 17:46:27 +0530

    Update PalindromeNo.cpp

--- basicMATHS/PalindromeNo.cpp
+++ basicMATHS/PalindromeNo.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int palindrome(int x){
+    if(x < 0) return false;
+    if(x != 0 and x%10 == 0) return false;
 
+    int reverse = 0;
+    while(x>reverse){
+        reverse = (reverse*10) + x%10;
+        x = x/10;
+    }
+    if(x == reverse){
+        return true;
+    }
+    if(x == reverse/10){
+        return true;
+    }
+    return false;
+}
+int main(){
+   int x;
+   cout << "Enter the number: ";
+   cin >> x;
+   palindrome(x);
+}
